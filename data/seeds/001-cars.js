73b6e15b8a6c868e2d38d0aec9c3661d0e27943a
Hui <jeffrey.x.hui@gmail.com>
2020-01-21 13:06:56 -0800

    built a seed and ran it!

---
+++ data/seeds/001-cars.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('cars').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cars').insert([
+        {VIN: '1234567890ABCDEFD', make: 'Toyota', model: 'Camry', mileage: 150},
+        {VIN: '1234567890ABCDEFE', make: 'Tesla', model: 'Model S', mileage: 250},
+        {VIN: '1234567890ABCDEFZ', make: 'Ford', model: 'BigBoi', mileage: 350}
+      ]);
+    });
+};
